d2d81cad5555f0592b7e6186af2814e96dc0a611
代从伟 <daicw@sit.com.cn>
2021-05-18 15:18:10 +0800

    echarts 按需导入

---
+++ src/components/c_echarts.vue
@@ -0,0 +1,31 @@
+<template>
+  <div :id="id" :style="{height:height, width:width}"></div>
+</template>
+
+<script>
+let echarts = require("echarts/lib/echarts");
+require("echarts/lib/chart/bar"); //柱状图
+// require("echarts/lib/chart/pie"); //饼图
+require("echarts/lib/chart/line"); //饼图
+//下面的是需要的提示框
+require("echarts/lib/component/tooltip");
+require("echarts/lib/component/title");
+require("echarts/lib/component/legend");
+
+import { GridComponent } from "echarts/components";
+echarts.use([GridComponent]);
+export default {
+  props: ["id", "options", "width", "height"],
+  mounted() {
+    this.$nextTick(() => {
+      this.draw(this.id); //调用
+    });
+  },
+  methods: {
+    draw(id) {
+      let myChart = echarts.init(document.getElementById(id));
+      myChart.setOption(this.options);
+    }
+  }
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Cecharts id="rcharts_1" height="300px" width="300px" :options="option" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import Cecharts from "@/components/c_echarts";
+
+export default {
+  name: "Home",
+  components: {
+    // HelloWorld
+    Cecharts
+  },
+  data() {
+    return {
+      option: {
+        legend: {},
+        tooltip: {},
+        dataset: {
+          source: [
+            ["product", "新增用户", "新增信用卡", "新增商户"],
+            ["Matcha Latte", 43.3, 85.8, 93.7],
+            ["Milk Tea", 83.1, 73.4, 55.1],
+            ["Cheese Cocoa", 86.4, 65.2, 82.5],
+            ["Walnut Brownie", 72.4, 53.9, 39.1]
+          ]
+        },
+        xAxis: { type: "category" },
+        yAxis: {},
+        // Declare several bar series, each will be mapped
+        // to a column of dataset.source by default.
+        series: [
+          { type: "line", color: "#f96f96" },
+          { type: "line", color: "#7794ec" },
+          { type: "line", color: "#efaf49" }
+        ]
+      }
+    };
+  }
+};
+</script>
